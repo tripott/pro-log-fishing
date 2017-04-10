ed3627e789e394a7ad67088058138bcef4bab675
tripott <tripott@gmail.com>
2017-04-10 08:55:35 -0400

    adding Fish components

---
+++ src/components/Fish.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {propOr} from 'ramda'
+
+const Fish = (props) => {
+
+  const name = propOr('N/A', 'fishName', props)
+  const time = propOr('', 'fishTime', props)
+  const stats = propOr('', 'fishStats', props)
+
+  return (
+    <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
+      <div className="dtc v-mid pl3">
+        <h1 className="f4 fw2 lh-title black mv0">{name} <span className="f4 fw2 mt0 mb0 black-60"> (12in - 1lb) </span>  @ {time} </h1>
+      </div>
+      <div className="dtc v-mid">
+        <form className="w-100 tr">
+          <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Add</button>
+        </form>
+      </div>
+    </article>
+  )
+}
+
+export default Fish
--- src/components/LogEntryDetail.js
+++ src/components/LogEntryDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { find, propEq, propOr, pathOr } from 'ramda'
+import { find, propEq, propOr, pathOr, map } from 'ramda'
 import moment from 'moment'
 import MapContainer from './MapContainer'
 import Rating from './Rating'
@@ -10,6 +10,7 @@ import Tide from './Tide'
 import WaterTemp from './WaterTemp'
 import Wind from './Wind'
 import Weather from './Weather'
+import Fish from './Fish'
 
 //GOOGLE API KEY:  AIzaSyA6PNXwjUhL0VD7WYutPfLVKILzvj74Y-8
 const LogEntryDetail = (props) => {
@@ -40,6 +41,10 @@ const LogEntryDetail = (props) => {
   const weatherUnits = pathOr('', ['weather','units'], logEntry)
   const weatherDesc = pathOr('', ['weather','desc'], logEntry)
 
+  const fishes = pathOr([], ['fishes'], logEntry)
+
+  const fishItems = map(fish => <Fish fishName={fish.species} fishTime={fish.time} fishStats={fish.stats} />, fishes)
+
   if (position === '') {
     return <div>Loading Map...</div>
   }
@@ -61,48 +66,7 @@ const LogEntryDetail = (props) => {
 
         <div className="pa2 pb3 fl w-100 tc bg-black-10">
           <main className="mw6 center">
-            <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-              <div className="dtc w3 w4-ns v-mid">
-                <img src="/fish-flounder.jpg" className="ba b--black-10 db " alt="flounder" />
-              </div>
-              <div className="dtc v-mid pl3">
-                <h1 className="f6 f5-ns fw6 lh-title black mv0">Flounder - 12in - 1lb</h1>
-                <h2 className="f6 fw4 mt0 mb0 black-60">1:05 PM</h2>
-              </div>
-              <div className="dtc v-mid">
-                <form className="w-100 tr">
-                  <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Add</button>
-                </form>
-              </div>
-            </article>
-            <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-                <div className="dtc w3 w4-ns v-mid">
-                  <img src="/fish-sea-trout.jpg" className="ba b--black-10 db " alt="trout" />
-                </div>
-                <div className="dtc v-mid pl3">
-                  <h1 className="f6 f5-ns fw6 lh-title black mv0">Trout - 12in - 1lb</h1>
-                  <h2 className="f6 fw4 mt0 mb0 black-60">1:15 PM</h2>
-                </div>
-                <div className="dtc v-mid">
-                  <form className="w-100 tr">
-                    <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Add</button>
-                  </form>
-                </div>
-              </article>
-            <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-                <div className="dtc w3 w4-ns v-mid">
-                  <img src="/fish-red-fish.jpg" className="ba b--black-10 db" alt="flounder" />
-                </div>
-                <div className="dtc v-mid pl3">
-                  <h1 className="f6 f5-ns fw6 lh-title black mv0">Redfish - 12in - 1lb</h1>
-                  <h2 className="f6 fw4 mt0 mb0 black-60">1:30 PM</h2>
-                </div>
-                <div className="dtc v-mid">
-                  <form className="w-100 tr">
-                    <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Add</button>
-                  </form>
-                </div>
-              </article>
+            {fishItems}
           </main>
         </div>
         <div>
